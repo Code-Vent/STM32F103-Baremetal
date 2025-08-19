51e3575c24842a6745e041771579ba7a23bace94
Code-Vent <oyeyiolahamid@yahoo.com>
2025-08-19 08:49:18 +0100

    Update clock.h

---
+++ src/clock.h
@@ -0,0 +1,24 @@
+#pragma once 
+#include<stdint.h>
+#include"def.h"
+
+
+struct rcc {
+	reg_type CR;
+	reg_type CFGR;
+	reg_type CIR;
+	reg_type APB2RSTR;
+	reg_type APB1RSTR;
+	reg_type AHBENR;
+	reg_type APB2ENR;
+	reg_type APB1ENR;
+	reg_type BDCR;
+	reg_type CSR;
+};
+
+typedef struct rcc rcc_t;
+typedef enum { AHB, APB1, APB2 }clock_sel_t;
+
+void clock_enable(rcc_t* rcc, uint8_t bit, clock_sel_t clk);
+void clock_init(rcc_t* rcc, uint32_t mhz);
+uint32_t clock_freq();
