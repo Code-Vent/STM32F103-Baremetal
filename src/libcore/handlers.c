c9ecbd391272584407469c72ac8c64fd5d693b02
Code-Vent <oyeyiolahamid@yahoo.com>
2025-08-21 18:40:30 +0100

    blinky

--- src/libcore/handlers.c
+++ src/libcore/handlers.c
@@ -2,27 +2,14 @@
 
 core_t* __core_ptr__;
 
-#define __get_LR() ({ \
-        uint32_t lr; \
-	__asm volatile( \
-		"mov %0, lr\n" \
-		: "=r"(lr)); \
-		lr; \
-	})
-
-
-void syscall_dispatcher() {
-    SvCallParams s = __core_ptr__->svc_params;
-    if (s.num < MAX_SYS_FUNC && __core_ptr__->kernel[s.num]) {
-        __core_ptr__->kernel[s.num](s.args);
-    }    
-}
-
 void SysTick_Handler() {
 	__core_ptr__->ticks++;
 }
 
 
 void SVC_Handler() {
-    syscall_dispatcher();
+    SvCallParams s = __core_ptr__->svc_params;
+    if (s.num < MAX_SYS_FUNC && __core_ptr__->kernel[s.num]) {
+        __core_ptr__->kernel[s.num](s.args);
+    }   
 }
